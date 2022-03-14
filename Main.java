9aadbd7141c4bc3ef7e02878dbfd309e3aeab16b
Nedoxy <nk.nedum@gmail.com>
2022-03-14 22:28:41 +0100

    Added Main.Java file

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World. This is my project.");
+    System.out.println("My name is CHINEDU ANYASOR.");
+    System.out.println("Nothing is impossible. The world itself says I'm possible.");
+    System.out.println("When I have a dream, I've to grab it and never let go.");
+    System.out.println("There is nothing impossible to they who will try. I'll always try.");
+    System.out.println("Success is not final, failure is not fatal: It is the courage to continue that counts.");
+    System.out.println("I can be everything. I can be the infinite amount of things that people are.");
+    System.out.println("No matter what people tell me, words and ideas can change the world.");
+    System.out.println("Believe you can and you are halfway there.");
+    System.out.println("Weaknesses are just strengths in the wrong environment.");
+    }
+}
+
+
+
